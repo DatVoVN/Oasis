a53871842f56da4672a1f897589f71fb3aa28f1d
DatVoVN <dat246642@gmail.com>
2025-01-04 02:27:15 +0700

    Last

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,36 +1,36 @@
-import React from "react"
-import { Outlet } from "react-router-dom"
-import Header from "./Header"
-import Sidebar from "./SideBar"
-import styled from "styled-components"
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "./Header";
+import SideBar from "./SideBar";
+import styled from "styled-components";
 const Main = styled.main`
   background-color: var(--color-grey-50);
   padding: 4rem 4.8rem 6.4rem;
   overflow: scroll;
-`
+`;
 const StyledAppLayout = styled.div`
   display: grid;
   grid-template-columns: 26rem 1fr;
   grid-template-rows: auto 1fr;
   height: 100vh;
-`
+`;
 const Container = styled.div`
   max-width: 120rem;
   margin: 0 auto;
   display: flex;
   flex-direction: column;
   gap: 3.2rem;
-`
+`;
 export default function AppLayout() {
   return (
     <StyledAppLayout>
       <Header />
-      <Sidebar />
+      <SideBar />
       <Main>
         <Container>
           <Outlet />
         </Container>
       </Main>
     </StyledAppLayout>
-  )
+  );
 }
