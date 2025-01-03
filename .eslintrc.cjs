3d56902d0867954bcd66ad2f4d4485472654f9d3
DatVoVN <dat246642@gmail.com>
2025-01-04 02:17:37 +0700

    Final

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,10 +13,11 @@ module.exports = {
   settings: { react: { version: "18.2" } },
   plugins: ["react-refresh"],
   rules: {
+    "react/prop-types": "off",
     "no-unused-vars": "warn",
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
     ],
   },
-}
+};
